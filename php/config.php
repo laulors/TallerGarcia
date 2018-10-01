0f5a8ef935a0d3aa9e7095b7fd6482a740a67378
Lorena García <laura_lors_145@hotmail.com>
2018-09-30 23:28:55 -0500

    trying to connect the database part 1.

---
+++ php/config.php
@@ -0,0 +1,11 @@
+<?php
+	function bd(){
+		$info = array(
+			'host' => 'localhost',
+			'dbname' => 'taller',
+			'user' => 'root',
+			'password' => ''
+		);
+		return $info;
+	}
+ ?>
--- php/signin.php
+++ php/signin.php
@@ -1,7 +1,5 @@
 <?php
 	if (isset($_POST['btnLogin'])) {
-		$x = "llego";
-		echo $x;
-	}
 
+	}
 ?>
